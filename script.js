2eb834542e736a38ceab478f35fb72644d07fc80
mishazapl <mzapalenov@mail.ru>
2022-06-14 18:06:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 100; i++) {
     setTimeout(function() {
         var confirmButtons = document.querySelectorAll('button[value=Confirm]');
         for (var x = 0; x < confirmButtons.length; x++) {
           setTimeout(function(confirmButtons, x) {    
             confirmButtons[x].firstChild.click();
-          }, x * 1000, confirmButtons, x); // we're passing x
+          }, x * 5000, confirmButtons, x); // we're passing x
         }
-    }, i * 10000);
+    }, i * 30000);
 }
